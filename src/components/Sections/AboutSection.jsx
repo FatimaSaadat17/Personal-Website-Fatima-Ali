6831fccb2de584584d0a19f0aba194813aba0a7a
FatimaAli <fsaadat917@gmail.com>
2026-09-10 16:19:16 +0400

    Update AboutSection.jsx

--- src/components/Sections/AboutSection.jsx
+++ src/components/Sections/AboutSection.jsx
@@ -69,7 +69,7 @@ export default function AboutSection() {
             <div className="focus-indicator" style={{ background: '#EFC96A' }} />
             <div>
               <strong>LinkedIn</strong>
-              <a href="https://www.linkedin.com/in/fatima-ali-0299163900">https://linkedin.com</a>
+              <a href="www.linkedin.com/in/fatima-ali-029916390">www.linkedin.com/in/fatima-ali-029916390</a>
             </div>
           </div>
 
